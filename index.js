6e4cb58f05a6d1327907e26943673b6ed0aee387
mdmahmudunnabi4 <mdmahmudunnabi4@gmail.com>
2024-02-07 09:01:47 +0600

    Learn JQuery html selector and text manipulation

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ $("h1").text("Hi")
 $(".my-div h1").text("Goodby")
 
 $('#p1').text("Hello")
+
+$('#p2').html("<b>Wellcom</b>")
+
+$('#p3').append(" <b>goodbye</b>")
+
+
+$('#p4').prepend("<b>goodbye</b> ")
+
+
+const myPara6 = $("<p></p>").text('This is paragraph 6')
+$("#p5").after(myPara6)
+
+const myPara7 = $("<p></p>").text('This is paragraph 7')
+$("#p6").before(myPara7)
